ba846beedda1071baa1e6e088e5df5853d5ab659
maizabros <39334631+maizabros@users.noreply.github.com>
2021-09-15 20:14:36 +0200

    Add files via upload

--- liberadAW.sh
+++ liberadAW.sh
@@ -3,13 +3,21 @@
 # Copy each answer and paste it folowed by enter (it will not show anything, but do it)
 # You only have to use 'ssh user@ip | ./liberadAW.sh'
 # Remember to give execution permission with 'chmod +x liberadAW.sh'
+fifo=fifo
+mkfifo ${fifo}
+exec 3<> ${fifo}
+function exit_prog {
+   rm ${fifo};
+   exit;
+}
+trap exit_prog INT
 
 BLUE='\033[0;36m'
 NC='\033[0m' # No Color
 CLAVE=""
 
 while read line; do
-   echo "$line"
+   echo -e "$line"
    if [[ $line == *'Bien hecho!!! Ya tienes'* ]]
    then
       break;
@@ -18,9 +26,10 @@ while read line; do
    if [[ $line == *'dámelo'* ]] 
    then
       read line;
-      echo $line;
+      echo -e "$line";
       CLAVE=`echo "$line" | openssl enc -base64 -d`;
       echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
+      echo "${CLAVE}" >&3;
    fi
    # Hashing stage
    if [[ $line == *'el hash'* ]]
@@ -28,23 +37,21 @@ while read line; do
       COD=`echo $line | cut -d " " -f 7`
       CLAVE=`echo -n $CLAVE | openssl dgst -$COD | cut -d " " -f 2`;
       echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
+      echo "${CLAVE}" >&3;
    fi
    if [[ $line == *'y como password'* ]]
    then
       COD=`echo $line | cut -d " " -f 7`
       PASS=`echo $line | cut -d " " -f 11 | cut -d "." -f 1`
-      read line; echo $line;
-      read line; echo $line;
-      read line; echo $line;
-      read line2; echo $line2;
+      read line; echo -e "$line";
+      read line; echo -e "$line";
+      read line; echo -e "$line";
+      read line2; echo -e "$line2";
       CLAVE=`echo -n $line; echo -n $line2`;
-      CLAVE=`echo "$CLAVE" | openssl enc -base64 -d | openssl enc -$COD -d -pass "pass:$PASS"`;
-      echo -e "$BLUE $CLAVE $NC";
-
-       
-
+      CLAVE=`echo "$CLAVE" | openssl enc -base64 -d | openssl enc -$COD -d -pass "pass:$PASS" | cut -d " " -f 7`;
+      echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
+      read line; echo -n $line;
+      echo "${CLAVE}" >&3;
    fi
-
-
-
-done
+done < <(ssh -tt $1@$2 <${fifo})
+exit_prog
