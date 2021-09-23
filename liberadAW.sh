699d9eb712faa7dd2b1132917a2dd9bfd58d93d7
maizabros <39334631+maizabros@users.noreply.github.com>
2021-09-23 21:08:18 +0200

    Comentados los echo para que no haya tanta mierda

--- liberadAW.sh
+++ liberadAW.sh
@@ -18,14 +18,14 @@ CLAVE=""
 FIRST=0;
 
 while read line; do
-   echo -e "${line}";
+   # echo -e "${line}";
    # First encoding
    if [[ $line == *'dámelo'* ]] 
    then
       read line;
-      echo -e "$line";
+      # echo -e "$line";
       CLAVE=`echo "$line" | openssl enc -base64 -d`;
-      echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
+      # echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
       echo "${CLAVE}" >&3;
    fi
    # Hashing stage
@@ -33,21 +33,21 @@ while read line; do
    then
       COD=`echo $line | cut -d " " -f 7`
       CLAVE=`echo -n $CLAVE | openssl dgst -$COD | cut -d " " -f 2`;
-      echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
+      # echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
       echo "${CLAVE}" >&3;
    fi
    if [[ $line == *'y como password'* ]]
    then
       COD=`echo $line | cut -d " " -f 7`
       PASS=`echo $line | cut -d " " -f 11 | cut -d "." -f 1`
-      read line; echo -e "$line";
-      read line; echo -e "$line";
-      read line; echo -e "$line";
-      read line2; echo -e "$line2";
+      read line; # echo -e "$line";
+      read line; # echo -e "$line";
+      read line; # echo -e "$line";
+      read line2; # echo -e "$line2";
       CLAVE=`echo -n $line; echo -n $line2`;
       CLAVE=`echo "$CLAVE" | openssl enc -base64 -d | openssl enc -$COD -d -pass "pass:$PASS" | cut -d " " -f 7`;
-      echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
-      read line; echo -n $line;
+      # echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
+      read line; # echo -n $line;
       echo "${CLAVE}" >&3;
    fi
 
@@ -65,7 +65,7 @@ while read line; do
       read line3; echo -e "$line3";
       CLAVE=`echo -n $line; echo -n $line2; echo -n $line3`;
       CLAVE=`echo "$CLAVE" | openssl enc -base64 -d | openssl rsautl -decrypt -inkey clave1.priv | cut -d " " -f 7`;
-      echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
+      # echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
       echo "${CLAVE}" >&3;
 
    fi
@@ -76,11 +76,11 @@ while read line; do
 
    if [[ $line == *'Bien, la password cifrada'* ]]; then
       if [[ $line == *'tras haber hecho'* ]]; then
-         read line; echo -e "$line";
+         read line; # echo -e "$line";
          COD=`echo $line | cut -d " " -f 10 | cut -d "." -f 1`;
       fi
-      read line; echo -e "$line";
-      read line2; echo -e "$line2";
+      read line; # echo -e "$line";
+      read line2; # echo -e "$line2";
       CLAVE=`echo -n $line; echo -n $line2`;
 
    fi
@@ -95,7 +95,7 @@ while read line; do
       PASS=`echo -n $line; echo -n $line2; echo -n $line3`;
       PASS=`echo "$PASS" | openssl enc -base64 -d | openssl rsautl -decrypt -inkey clave1.priv | cut -d " " -f 5`;
       CLAVE=`echo "$CLAVE" | openssl enc -base64 -d | openssl enc -$COD -d -pass "pass:$PASS" | cut -d " " -f 7`;
-      echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
+      # echo -e "$BLUE Aquí tienes joven padawan: $CLAVE $NC";
       read line; echo -n $line;
       echo "${CLAVE}" >&3;
 
@@ -111,23 +111,37 @@ while read line; do
       wget $WEBDIR/clave{1..3}.pub 2> /dev/null;
       wget $WEBDIR/clave{1..3}.priv 2> /dev/null;
       wget $WEBDIR/file{1..5} 2> /dev/null;
-      read line; echo -e "$line"; read line2; echo -e "$line2"; read line3; echo -e "$line3";
+      read line; # echo -e "$line"; 
+      read line2; # echo -e "$line2"; 
+      read line3; # echo -e "$line3";
       KEY1=`echo -n $line; echo -n $line2; echo -n $line3`;
       echo "$KEY1" | openssl enc -base64 -d > key1
-      read line; echo -e "$line"; read line; echo -e "$line";
-      read line; echo -e "$line"; read line2; echo -e "$line2"; read line3; echo -e "$line3";
+      read line; # echo -e "$line"; 
+      read line; # echo -e "$line";
+      read line; # echo -e "$line";
+      read line2; # echo -e "$line2"; 
+      read line3; # echo -e "$line3";
       KEY2=`echo -n $line; echo -n $line2; echo -n $line3`;
       echo "$KEY2" | openssl enc -base64 -d > key2
-      read line; echo -e "$line"; read line; echo -e "$line";
-      read line; echo -e "$line"; read line2; echo -e "$line2"; read line3; echo -e "$line3";
+      read line; # echo -e "$line"; 
+      read line; # echo -e "$line";
+      read line; # echo -e "$line"; 
+      read line2; # echo -e "$line2";
+      read line3; # echo -e "$line3";
       KEY3=`echo -n $line; echo -n $line2; echo -n $line3`;
       echo "$KEY3" | openssl enc -base64 -d > key3
-      read line; echo -e "$line"; read line; echo -e "$line";
-      read line; echo -e "$line"; read line2; echo -e "$line2"; read line3; echo -e "$line3";
+      read line; # echo -e "$line"; 
+      read line; # echo -e "$line";
+      read line; # echo -e "$line"; 
+      read line2; # echo -e "$line2";
+      read line3; # echo -e "$line3";
       KEY4=`echo -n $line; echo -n $line2; echo -n $line3`;
       echo "$KEY4" | openssl enc -base64 -d > key4
-      read line; echo -e "$line"; read line; echo -e "$line";
-      read line; echo -e "$line"; read line2; echo -e "$line2"; read line3; echo -e "$line3";
+      read line; # echo -e "$line"; 
+      read line; # echo -e "$line";
+      read line; # echo -e "$line"; 
+      read line2; # echo -e "$line2";
+      read line3; # echo -e "$line3";
       KEY5=`echo -n $line; echo -n $line2; echo -n $line3`;
       echo "$KEY5" | openssl enc -base64 -d > key5
     fi
@@ -178,19 +192,19 @@ while read line; do
     fi
    # Skip first steps
    if [[ $line == *'please try'* ]]; then
-      echo -e "${line}";
+      # echo -e "${line}";
       echo $'\n' >&3;
 
    fi
    if [[ $line == *'Ahora debes morir.'* ]]; then
-      echo -e "${line}";
+      # echo -e "${line}";
       echo $'\003' >&3;
       FIRST=-1
    fi
    # Skip false game end
    if [ $FIRST = -1 ]; then
       if [[ $line == *'Ahora adentro y securiza'* ]]; then
-         echo -e "${line}";
+         # echo -e "${line}";
          echo $'\n\n\n' >&3;
       fi
    fi
